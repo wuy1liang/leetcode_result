297910e9c9674535654760abe422150705b437d8
wuyiliang <771134168@qq.com>
2020-07-20 18:03:12 +0800

    1道

--- src/com/leecode/result2020/LeetCode07.java
+++ src/com/leecode/result2020/LeetCode07.java
@@ -297,4 +297,22 @@ public class LeetCode07 {
         }
         return left;
     }
+
+    /**
+     *  https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted/
+     */
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while (left < right) {
+            if (numbers[left] + numbers[right] > target) {
+                right--;
+            } else if (numbers[left] + numbers[right] < target) {
+                left++;
+            } else {
+                break;
+            }
+        }
+        return new int[]{left + 1, right + 1};
+    }
 }
