247cc5626cc4b43b87a1704f9aee926271855bc8
wuyiliang <771134168@qq.com>
2020-07-27 08:35:29 +0800

    1道

--- src/com/leecode/result2020/LeetCode07.java
+++ src/com/leecode/result2020/LeetCode07.java
@@ -315,4 +315,18 @@ public class LeetCode07 {
         }
         return new int[]{left + 1, right + 1};
     }
+
+    /**
+     *  https://leetcode-cn.com/problems/is-subsequence/
+     */
+    public boolean isSubsequence(String s, String t) {
+        int index = -1;
+        for (char c : s.toCharArray()) {
+            index = t.indexOf(c, index + 1);
+            if (index == -1) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
