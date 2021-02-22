3d1f4a25be980b5da119a782c20872915a6e9fc0
wuyiliang <771134168@qq.com>
2021-02-22 14:19:02 +0800

    每日一题

--- src/com/leecode/_2021/February.java
+++ src/com/leecode/_2021/February.java
@@ -5,4 +5,22 @@ package com.leecode._2021;
  * @date 2021/2/22 11:34
  */
 public class February {
+
+    /**
+     * https://leetcode-cn.com/problems/toeplitz-matrix/
+     */
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] != matrix[i - 1][j - 1]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
