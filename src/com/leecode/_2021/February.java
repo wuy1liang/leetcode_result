39b565d4804a8d9bb4464bd5543b512c3bc662e3
wuyiliang <771134168@qq.com>
2021-02-25 17:17:54 +0800

    init

--- src/com/leecode/_2021/February.java
+++ src/com/leecode/_2021/February.java
@@ -23,4 +23,21 @@ public class February {
 
         return true;
     }
+
+    /**
+     * https://leetcode-cn.com/problems/transpose-matrix/
+     */
+    public int[][] transpose(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] result = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                result[i][j] = matrix[j][i];
+            }
+        }
+
+        return result;
+    }
 }
