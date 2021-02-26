6c4aa9a024b762b0c6d540d66afbfa661542a1f3
wuyiliang <771134168@qq.com>
2021-02-26 13:48:09 +0800

    1道

--- src/com/leecode/_2021/February.java
+++ src/com/leecode/_2021/February.java
@@ -1,5 +1,6 @@
 package com.leecode._2021;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -47,6 +48,18 @@ public class February {
      * https://leetcode-cn.com/problems/subsets/
      */
     public List<List<Integer>> subsets(int[] nums) {
-
+        List<Integer> temp = new ArrayList<>();
+        dfs(nums, temp, 0);
+        res.add(new ArrayList<>());
+        return res;
+    }
+    public List<List<Integer>> res = new ArrayList<>();
+    public void dfs(int[] nums, List<Integer> temp, int len) {
+        for (int i = len; i < nums.length; i++) {
+            temp.add(nums[i]);
+            res.add(new ArrayList<>(temp));
+            dfs(nums, temp, i + 1);
+            temp.remove(temp.size() - 1);
+        }
     }
 }
