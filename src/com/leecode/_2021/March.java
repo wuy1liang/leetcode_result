19fa8c20d19a5097f680b5c52b5724f84d07609a
wuyiliang <771134168@qq.com>
2021-03-22 00:48:34 +0800

    1

--- src/com/leecode/_2021/March.java
+++ src/com/leecode/_2021/March.java
@@ -157,4 +157,16 @@ public class March {
             }
         }
     }
+
+    /**
+     * https://leetcode-cn.com/problems/number-of-1-bits/
+     */
+    public int hammingWeight(int n) {
+        int result = 0;
+        while (n != 0) {
+            n &= n - 1;
+            result++;
+        }
+        return result;
+    }
 }
