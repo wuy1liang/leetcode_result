16dd09b3b0eb659d07b44aaec59ef9dadf322b65
wuyiliang <771134168@qq.com>
2021-03-24 18:51:04 +0800

    1

--- src/com/leecode/_2021/March.java
+++ src/com/leecode/_2021/March.java
@@ -247,4 +247,23 @@ public class March {
             return this.iterator.hasNext();
         }
     }
+
+    /**
+     * https://leetcode-cn.com/problems/132-pattern/
+     */
+    public boolean find132pattern(int[] nums) {
+        int n = nums.length;
+        Deque<Integer> d = new ArrayDeque<>();
+        int k = Integer.MIN_VALUE;
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] < k) {
+                return true;
+            }
+            while (!d.isEmpty() && d.peekLast() < nums[i]) {
+                k = d.pollLast();
+            }
+            d.addLast(nums[i]);
+        }
+        return false;
+    }
 }
