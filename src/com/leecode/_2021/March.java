9cb7208b00b2bba01de7a695542d413a4a690529
wuyiliang <771134168@qq.com>
2021-03-29 20:56:28 +0800

    1

--- src/com/leecode/_2021/March.java
+++ src/com/leecode/_2021/March.java
@@ -296,4 +296,18 @@ public class March {
         }
         return start.next;
     }
+
+    /**
+     * https://leetcode-cn.com/problems/reverse-bits/
+     */
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            int t = (n >> i) & 1;
+            if (t == 1) {
+                result |= (1 << (31 - i));
+            }
+        }
+        return result;
+    }
 }
