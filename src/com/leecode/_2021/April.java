b095e0e935c45c39bbc0a911d55aacb815fb223a
wuyiliang <771134168@qq.com>
2021-04-21 11:51:16 +0800

    1

--- src/com/leecode/_2021/April.java
+++ src/com/leecode/_2021/April.java
@@ -386,4 +386,36 @@ public class April {
         }
         return left;
     }
+
+    /**
+     * https://leetcode-cn.com/problems/decode-ways/
+     */
+    public int numDecodings(String s) {
+        int len = s.length();
+        int[] dp = new int[len + 1];
+        int value = s.charAt(0) - '0';
+        if (value == 0) {
+            return 0;
+        }
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= len; i++) {
+            int cur = s.charAt(i - 1) - '0';
+            int pre = s.charAt(i - 2) - '0';
+            if (cur == 0) {
+                if (pre == 1 || pre == 2) {
+                    dp[i] = dp[i - 2];
+                } else {
+                    return 0;
+                }
+            } else {
+                if (pre == 1 || (cur <= 6 && pre == 2)) {
+                    dp[i] = dp[i - 1] + dp[i - 2];
+                } else {
+                    dp[i] = dp[i - 1];
+                }
+            }
+        }
+        return dp[len];
+    }
 }
