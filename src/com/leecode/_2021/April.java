4a9384ed451e09fe964f4d0b30e7f55d954174fb
wuyiliang <771134168@qq.com>
2021-04-24 10:26:54 +0800

    1

--- src/com/leecode/_2021/April.java
+++ src/com/leecode/_2021/April.java
@@ -576,4 +576,20 @@ public class April {
             dfsConvertBST(root.left);
         }
     }
+
+    /**
+     * https://leetcode-cn.com/problems/combination-sum-iv/
+     */
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (i >= num) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        return dp[target];
+    }
 }
