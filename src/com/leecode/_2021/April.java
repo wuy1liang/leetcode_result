e49b66bce74ce2c16de3ddbfddf2b88fd3e52945
wuyiliang <771134168@qq.com>
2021-04-26 22:55:55 +0800

    2

--- src/com/leecode/_2021/April.java
+++ src/com/leecode/_2021/April.java
@@ -692,4 +692,48 @@ public class April {
         }
         return right;
     }
+
+    /**
+     * https://leetcode-cn.com/problems/unique-paths/
+     */
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i > 0) {
+                    dp[i][j] += dp[i - 1][j];
+                }
+                if (j > 0) {
+                    dp[i][j] += dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    /**
+     * https://leetcode-cn.com/problems/unique-paths-ii/
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        if (obstacleGrid[0][0] == 0) {
+            dp[0][0] = 1;
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    if (i > 0 && obstacleGrid[i - 1][j] == 0) {
+                        dp[i][j] += dp[i - 1][j];
+                    }
+                    if (j > 0 && obstacleGrid[i][j - 1] == 0) {
+                        dp[i][j] += dp[i][j - 1];
+                    }
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
 }
